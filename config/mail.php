23893e2515b1033c0337a94d289666f61760e892
Syed Galib Basha <syedsunnygalibbasha@gmail.com>
2026-05-17 12:59:12 +0530

    Added Resend Email sender for deployment comfort

--- config/mail.php
+++ config/mail.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'log'),
+    'default' => env('MAIL_MAILER', 'resend'),
 
     /*
     |--------------------------------------------------------------------------
@@ -36,7 +36,7 @@ return [
     */
 
     'mailers' => [
-
+     
         'smtp' => [
             'transport' => 'smtp',
             'scheme' => env('MAIL_SCHEME'),
